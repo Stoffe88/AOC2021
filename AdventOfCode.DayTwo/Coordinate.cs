8dddeaceb623d973876923e0aeb4604efe01d983
Kristoffer Kallerup <kristoffer.a.kallerup@sogeti.se>
2021-12-02 09:57:21 +0100

    minimized

--- AdventOfCode.DayTwo/Coordinate.cs
+++ AdventOfCode.DayTwo/Coordinate.cs
@@ -4,62 +4,41 @@ namespace AdventOfCode.DayTwo
 {
     public class Coordinate
     {
-        private int horizontal;
-        private int depth;
-        private int aim;
+        private int simpleHorizontal = 0;
+        private int horizontal = 0;
+        private int simpleDepth = 0;
+        private int depth = 0;
+        private int aim = 0;
 
-        public Coordinate(int horizontal, int depth)
-        {
-            this.horizontal = horizontal;
-            this.depth = depth;
-            this.aim = 0;
-        }
         public Coordinate()
-        {
-            this.horizontal = 0;
-            this.depth = 0;
-            this.aim = 0;
-        }
+        { }
 
         internal void ExecuteInstruction(MoveInstruction instruction)
         {
             switch (instruction.GetDirection())
             {
                 case "forward":
-                    this.horizontal += instruction.GetDirectionLenght();
-                    break;
-                case "up":
-                    this.depth -= instruction.GetDirectionLenght();
-                    break;
-                case "down":
-                    this.depth += instruction.GetDirectionLenght();
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        internal void ExecuteAimedInstruction(MoveInstruction instruction)
-        {
-            switch (instruction.GetDirection())
-            {
-                case "forward":
+                    this.simpleHorizontal += instruction.GetDirectionLenght();
                     this.horizontal += instruction.GetDirectionLenght();
                     this.depth += instruction.GetDirectionLenght() * this.aim;
                     break;
                 case "up":
+                    this.simpleDepth -= instruction.GetDirectionLenght();
                     this.aim -= instruction.GetDirectionLenght();
                     break;
                 case "down":
+                    this.simpleDepth += instruction.GetDirectionLenght();
                     this.aim += instruction.GetDirectionLenght();
                     break;
                 default:
                     break;
             }
         }
-
         internal int GetMultipliedValue()
         {
+            return this.simpleDepth * this.simpleHorizontal;
+        }
+        internal int GetAimedMultipliedValue(){
             return this.depth * this.horizontal;
         }
     }
--- AdventOfCode.DayTwo/Program.cs
+++ AdventOfCode.DayTwo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 
 namespace AdventOfCode.DayTwo
@@ -7,43 +6,17 @@ namespace AdventOfCode.DayTwo
     class Program
     {
         private static Coordinate coordinate = new Coordinate();
-        private static Coordinate aimedCoordinate = new Coordinate();
 
         static void Main(string[] args)
         {
-            List<MoveInstruction> moveInstructions = new List<MoveInstruction>();
-
             foreach (string line in File.ReadAllLines(@"C:\Users\kkalleru\source\repos\Personal\advent_of_code\AdventOfCode\AdventOfCode.DayTwo\input.txt"))
             {
-                moveInstructions.Add(new MoveInstruction(instructions: line));
+                coordinate.ExecuteInstruction(new MoveInstruction(instructions: line));
             };
-
-            foreach (MoveInstruction instruction in moveInstructions)
-            {
-                coordinate.ExecuteInstruction(instruction);
-                aimedCoordinate.ExecuteAimedInstruction(instruction);
-            }
 
             Console.WriteLine($"Value of horizontal value multiplied with depth value: {coordinate.GetMultipliedValue()}");
-            Console.WriteLine($"Value of horizontal value multiplied with depth value for aimed coordinate: {aimedCoordinate.GetMultipliedValue()}");
+            Console.WriteLine($"Value of horizontal value multiplied with aimed depth value: {coordinate.GetAimedMultipliedValue()}");
 
-            Coordinate testCoordinate = new Coordinate();
-
-            List<MoveInstruction> instructions = new List<MoveInstruction>
-            {
-                new MoveInstruction("forward 5"),
-                new MoveInstruction("down 5"),
-                new MoveInstruction("forward 8"),
-                new MoveInstruction("up 3"),
-                new MoveInstruction("down 8"),
-                new MoveInstruction("forward 2")
-            };
-            foreach (var item in instructions)
-            {
-                testCoordinate.ExecuteAimedInstruction(item);
-            }
-
-            Console.WriteLine($"Value of test data: {testCoordinate.GetMultipliedValue()}");
             Console.ReadKey();
         }
 
