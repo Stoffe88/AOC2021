2a3615d1e9714880921abd563860d11927d9d0f2
Kristoffer Kallerup <kristoffer.a.kallerup@sogeti.se>
2021-12-07 21:36:52 +0100

    Day6 done

---
+++ AdventOfCode.06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode._06
+{
+    class Program
+    {
+        internal static School school = new School();
+        static void Main(string[] args)
+        {
+            school.CreateSchool(File.ReadAllText(@"C:\Users\kkalleru\source\repos\Personal\advent_of_code\AdventOfCode\AdventOfCode.06\input.txt"));
+            Console.WriteLine($"On day 80 the school would be {school.CountOnDay(80)} fish big.");
+            
+            school.CreateSchool(File.ReadAllText(@"C:\Users\kkalleru\source\repos\Personal\advent_of_code\AdventOfCode\AdventOfCode.06\input.txt"));
+            Console.WriteLine($"On day 256 the school would be {school.CountOnDay(256)} fish big.");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode.06/School.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._06
+{
+    internal class School
+    {
+        private long[] fishs = new long[9];
+        public School()
+        {}
+
+        internal void CreateSchool(string fishData)
+        {
+            this.fishs = new long[9];
+            fishData.Split(',').ToList().ConvertAll(x => int.Parse(x))
+                .GroupBy(x => x)
+                .ToList()
+                .ForEach(x => this.fishs[x.Key] = x.Count());
+        }
+
+        internal long CountOnDay(int days)
+        {
+            for (int i = 0; i < days; i++)
+            {
+                long[] newFish = new long[9];
+                for (int j = 0; j < this.fishs.Length; j++)
+                {
+                    if (j == 0) { 
+                        newFish[8] = this.fishs[j];
+                        newFish[6] += this.fishs[j];
+                    }
+                    else newFish[j - 1] += this.fishs[j];
+                }
+                this.fishs = newFish;
+            }
+            return this.fishs.Sum();
+        }
+    }
+}
